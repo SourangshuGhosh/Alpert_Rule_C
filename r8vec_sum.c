be4f5e1ac344a04d829d3531d9409bc534c31ce6
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-23 20:49:58 +0530

    Create r8vec_sum.c

---
+++ r8vec_sum.c
@@ -0,0 +1,53 @@
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+# include "alpert_rule.h"
+
+/******************************************************************************/
+
+double r8vec_sum ( int n, double a[] )
+
+/******************************************************************************/
+/*
+  Purpose:
+
+    R8VEC_SUM returns the sum of an R8VEC.
+
+  Discussion:
+
+    An R8VEC is a vector of R8's.
+
+  Licensing:
+
+    This code is distributed under the GNU LGPL license.
+
+  Modified:
+
+    26 August 2008
+
+  Author:
+
+    John Burkardt
+
+  Parameters:
+
+    Input, int N, the number of entries in the vector.
+
+    Input, double A[N], the vector.
+
+    Output, double R8VEC_SUM, the sum of the vector.
+*/
+{
+  int i;
+  double value;
+
+  value = 0.0;
+  for ( i = 0; i < n; i++ )
+  {
+    value = value + a[i];
+  }
+
+  return value;
+}
