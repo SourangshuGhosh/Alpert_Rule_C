5269dc5273e1f9df331847bc3ed8bf9a96b3ba26
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-23 20:48:56 +0530

    Create r8vec_copy.c

---
+++ r8vec_copy.c
@@ -0,0 +1,50 @@
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+# include "alpert_rule.h"
+
+/******************************************************************************/
+
+void r8vec_copy ( int n, double a1[], double a2[] )
+
+/******************************************************************************/
+/*
+  Purpose:
+
+    R8VEC_COPY copies an R8VEC.
+
+  Discussion:
+
+    An R8VEC is a vector of R8's.
+
+  Licensing:
+
+    This code is distributed under the GNU LGPL license.
+
+  Modified:
+
+    03 July 2005
+
+  Author:
+
+    John Burkardt
+
+  Parameters:
+
+    Input, int N, the number of entries in the vectors.
+
+    Input, double A1[N], the vector to be copied.
+
+    Input, double A2[N], the copy of A1.
+*/
+{
+  int i;
+
+  for ( i = 0; i < n; i++ )
+  {
+    a2[i] = a1[i];
+  }
+  return;
+}
