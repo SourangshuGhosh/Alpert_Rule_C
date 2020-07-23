d153179c8bbbcfa066da699f6f3c6168edbfce3a
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-23 20:49:13 +0530

    Create r8vec_dot_product.c

---
+++ r8vec_dot_product.c
@@ -0,0 +1,48 @@
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+# include "alpert_rule.h"
+
+/******************************************************************************/
+
+double r8vec_dot_product ( int n, double a1[], double a2[] )
+
+/******************************************************************************/
+/*
+  Purpose:
+
+    R8VEC_DOT_PRODUCT computes the dot product of a pair of R8VEC's.
+
+  Licensing:
+
+    This code is distributed under the GNU LGPL license.
+
+  Modified:
+
+    26 July 2007
+
+  Author:
+
+    John Burkardt
+
+  Parameters:
+
+    Input, int N, the number of entries in the vectors.
+
+    Input, double A1[N], A2[N], the two vectors to be considered.
+
+    Output, double R8VEC_DOT_PRODUCT, the dot product of the vectors.
+*/
+{
+  int i;
+  double value;
+
+  value = 0.0;
+  for ( i = 0; i < n; i++ )
+  {
+    value = value + a1[i] * a2[i];
+  }
+  return value;
+}
