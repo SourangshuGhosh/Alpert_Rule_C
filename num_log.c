100728b2ede060a85e4a6ce36027623bec06fcce
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-23 20:45:57 +0530

    Create num_log.c

---
+++ num_log.c
@@ -0,0 +1,40 @@
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+# include "alpert_rule.h"
+
+/******************************************************************************/
+
+int num_log ( )
+
+/******************************************************************************/
+/*
+  Purpose:
+
+    NUM_LOG returns the number of Alpert rules for log singular functions.
+
+  Licensing:
+
+    This code is distributed under the GNU LGPL license.
+
+  Modified:
+
+    04 December 2015
+
+  Author:
+
+    John Burkardt
+
+  Parameters:
+
+    Output, int NUM_LOG, the number of rules.
+*/
+{
+  int num;
+
+  num = 10;
+
+  return num;
+}
