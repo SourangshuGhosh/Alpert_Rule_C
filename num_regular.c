ab33546e39d677317d93920a0af5b17a4eac1a73
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-23 20:46:39 +0530

    Create num_regular.c

---
+++ num_regular.c
@@ -0,0 +1,40 @@
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+# include "alpert_rule.h"
+
+/******************************************************************************/
+
+int num_regular ( )
+
+/******************************************************************************/
+/*
+  Purpose:
+
+    NUM_REGULAR returns the number of Alpert rules for regular functions.
+
+  Licensing:
+
+    This code is distributed under the GNU LGPL license.
+
+  Modified:
+
+    04 December 2015
+
+  Author:
+
+    John Burkardt
+
+  Parameters:
+
+    Output, int NUM_REGULAR, the number of rules.
+*/
+{
+  int num;
+
+  num = 12;
+
+  return num;
+}
